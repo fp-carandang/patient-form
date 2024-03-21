d211176114b1da540effeb7abcda0207960ab37f
fp-carandang <149746218+fp-carandang@users.noreply.github.com>
2024-03-21 11:04:39 +0800

    Delete p4-node-app directory

--- p4-node-app/backend/factories/UserFactory.js
+++
@@ -1,9 +0,0 @@
-import User from '../models/User.js';
-
-class UserFactory {
-  create(username, password, email) {
-    return new User({ username, password, email });
-  }
-}
-
-export default UserFactory;
